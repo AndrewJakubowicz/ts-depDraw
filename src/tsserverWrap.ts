15442e82af09511f74e70e8a80f77cca87c7edc7
SpyR1014 <spyr1014@gmail.com>
2016-12-01 10:55:39 +1100

    Refactor passing tests - request now saved

--- src/tsserverWrap.ts
+++ src/tsserverWrap.ts
@@ -13,7 +13,7 @@ winston.level = "verbose";
  */
 export class Tsserver{
     private proc: child_process.ChildProcess;
-    private operations: ((err: Error, s: string | Buffer)=>void)[] = [];
+    private operations: [((err: Error, response: string | Buffer, request: string)=>void), string][] = [];
     private seq: number = 0;                                                // tsserver requires everything to have a sequence number.
 
     /**
@@ -36,16 +36,16 @@ export class Tsserver{
                 return !(v === '' || v.slice(0, 14) === 'Content-Length')} );
 
             // Grab first callback and data.
-            let callback = this.operations.shift(),
+            let [callback, command] = this.operations.shift(),
                 chunk = allData.shift();
             
             while (allData.length > 0){
                 winston.log("debug", `Tsserver response: Checking lengths of operations vs callbacks: (${allData.length} == ${this.operations.length})`);
-                callback(null, chunk);
-                callback = this.operations.shift();
+                callback(null, chunk, command);
+                [callback, command] = this.operations.shift();
                 chunk = allData.shift();
             }
-            callback(null, chunk);
+            callback(null, chunk, command);
         });
 
 
@@ -55,8 +55,8 @@ export class Tsserver{
          */
         this.proc.stderr.on("data", d => {
             winston.log("error", `TSSERVER ERR: ${d}`);
-            let callback = this.operations.shift();
-            callback(new Error(d.toString()), null);
+            let [callback, command] = this.operations.shift();
+            callback(new Error(d.toString()), null, command);
         });
 
 
@@ -72,18 +72,18 @@ export class Tsserver{
      * This will store a callback on the FIFO queue.
      * If you don't open the file before trying to find definitions in it, this will fail.
      */
-    definition(filePath:string, line: number, column: number, callback: (err: Error, response: string )=> void) {
+    definition(filePath:string, line: number, column: number, callback: (err: Error, response: string, request: string )=> void) {
         let command = `{"seq":${this.seq},"type":"quickinfo","command":"definition","arguments":{"file":"${filePath}", "line":${line}, "offset": ${column}}}\n`;
         winston.log("debug", `SENDING TO TSSERVER: "${command}"`);
         this.proc.stdin.write(command);
-        this.operations.push(callback);
+        this.operations.push([callback, command]);
         this.seq++;
     }
-    open(filePath: string, callback: (err: Error, response: string )=> void){
+    open(filePath: string, callback: (err: Error, response: string, request: string )=> void){
         let command = `{"seq":${this.seq},"type":"request","command":"open","arguments":{"file":"${filePath}"}}\n`;
         winston.log("debug", `SENDING TO TSSERVER: "${command}"`);
         this.proc.stdin.write(command);
-        this.operations.push(callback);
+        this.operations.push([callback, command]);
         this.seq++;
     }
     kill(){
