23cdeeea430d3757c49b56695a5ab92efa06a443
SpyR1014 <spyr1014@gmail.com>
2016-12-02 14:10:43 +1100

    made the examples more interesting

--- tests/examples/ex4.ts
+++ tests/examples/ex4.ts
@@ -4,3 +4,8 @@ import * as example from "./ex3";
 export function betterAdder(c, d){
     return example.adderTest(2, 3);
 }
+
+
+export function betterConsoleLog(a){
+    // Oh shit a same function somewhere else.
+}
--- tests/examples/ex5.ts
+++ tests/examples/ex5.ts
@@ -2,7 +2,7 @@ import * as ex4 from "./ex4";
 
 ex4.betterAdder(2, 3);
 
-
+ex4.betterConsoleLog(3);
 
 export function betterConsoleLog(a){
     console.log(a);
