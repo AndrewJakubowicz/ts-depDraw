ffcb475946e84b33c27e7ee60043045eaa00db58
SpyR1014 <spyr1014@gmail.com>
2016-12-05 15:19:16 +1100

    Added promise polyfill

---
+++ static/src/clientCode.ts
@@ -0,0 +1,24 @@
+
+/**
+ * Author: Andrew Jakubowicz
+ */
+
+function delay(milliseconds: number) {
+    return new Promise<void>(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+
+async function dramaticWelcome() {
+    console.log("Hello");
+
+    for (let i = 0; i < 3; i++) {
+        await delay(500);
+        console.log(".");
+    }
+
+    console.log("WORLD!");
+}
+
+
+dramaticWelcome();
