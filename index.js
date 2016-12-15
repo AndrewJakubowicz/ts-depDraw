f2e70c280bfdd8038b26f687a35a51a7dc9a429a
SpyR1014 <spyr1014@gmail.com>
2016-12-15 14:51:46 +1100

    reset port number to 8080

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ var winston = require('./lib/appLogger');
 
 // Port defined
 // TODO: add to config.
-const PORT = 8085;
+const PORT = 8080;
 
 
-
-// Server testing
+// Server start and stop: This is basically the whole program.
 if (global.startServer) {
     let server = require('./lib/server')
         .SERVER
@@ -24,12 +23,13 @@ if (global.startServer) {
             }
             console.log(`Server started and listening on port: ${PORT}`);
         })
+        // On error close the program, with a helpful error message.
         .on('error', function (err) {
             if (err.message.includes('EADDRINUSE')) {
                 winston.log(`error`, `Port: ${PORT} is in use. Please change the port.`);
             }
             winston.log('trace', `Failed to start server with:`, err);
-            server.close(function() {
+            server.close(function () {
                 process.exit(0);
             });
         });
